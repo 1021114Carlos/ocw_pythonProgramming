cddc87042217e08a90d3c5a75d6f3502681d5bec
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-04-04 08:20:32 -0600

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,15 @@
+def search(keyword) :
+
+    # implement the function here
+    All_words = [Keyword]
+    for Entry in Thesaurus:
+        if Keyword = Entry.word:
+            for Word in Entry.synonym:
+                All_words.append(synonym)
+
+    return # modify to return a list of tuples
+
+input = "happy"
+output = search(input) # invoke the method using a test input
+print(output) # prints the output of the function
+# do not remove this line!

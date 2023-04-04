b3db7c30b599093588f8d2a2cbda417c28f17e04
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-04-04 09:40:53 -0600

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,13 +1,34 @@
 def search(keyword) :
-
     # implement the function here
-    All_words = [Keyword]
-    for Entry in Thesaurus:
-        if Keyword = Entry.word:
-            for Word in Entry.synonym:
-                All_words.append(synonym)
+   class Entry:
+      def __init__(self, word, synonyms):
+         self.word = word
+         self.synonyms = synonyms
+   #e1 = Entry("dog", ["doggie", "puppy"])
+   #e2 = Entry("cat", ["kitty"])
 
-    return # modify to return a list of tuples
+   #Thesaurus = [e1, e2]
+
+   #doc1 = ["this", "is", "a", "single", "document"]
+   #doc2 = ["here", "is", "another", "document"]
+
+   #Corpus = [doc1, doc2]
+
+   All_words = [keyword]
+   for Entry in Thesaurus:
+      if keyword == Entry.word:
+         for word in Entry.synonyms:
+            All_words.append(word)
+         break
+   for search_word in All_words:
+      count = 0
+      for document in Corpus:
+         for word in document:
+            if search_word == word:
+               count += 1
+      #return search_word, count
+
+   return search_word, count
 
 input = "happy"
 output = search(input) # invoke the method using a test input

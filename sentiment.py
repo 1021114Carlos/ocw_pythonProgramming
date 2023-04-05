cb11a430aa94ea8a7b25d225c43aa2da0728eb4b
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-04-05 09:30:27 -0600

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,34 +1,36 @@
-def search(keyword) :
-    # implement the function here
-   class Entry:
-      def __init__(self, word, synonyms):
-         self.word = word
-         self.synonyms = synonyms
-   #e1 = Entry("dog", ["doggie", "puppy"])
-   #e2 = Entry("cat", ["kitty"])
+def search(keyword):
+    #implement the function here
+    class Entry:
+        def __init__(self, word, synonyms):
+            self.word = word
+            self.synonyms = synonyms
+            
+    e1 = Entry("dog", ["cheerful", "mad", "delighted"])
+    e2 = Entry("nature", ["tiger", "waterfall", "joyful"])
 
-   #Thesaurus = [e1, e2]
+    Thesaurus = [e1, e2]
+    
+    doc1 = ["this", "is", "a", "single", "document"]
+    doc2 = ["here", "is", "another", "document"]
+    
+    Corpus = [doc1, doc2]
+    
+    word_list = []
+    All_words = [keyword]
+    for Entry in Thesaurus:
+        if keyword == Entry.word:
+            if word in Entry.synonyms:
+                All_words.append(word)
+            break
+    for search_word in All_words:
+        count = 0
+        for document in Corpus:
+            for word in document:
+                if search_word == word:
+                    count += 1
+                    word_list.append(search_word)
 
-   #doc1 = ["this", "is", "a", "single", "document"]
-   #doc2 = ["here", "is", "another", "document"]
-
-   #Corpus = [doc1, doc2]
-
-   All_words = [keyword]
-   for Entry in Thesaurus:
-      if keyword == Entry.word:
-         for word in Entry.synonyms:
-            All_words.append(word)
-         break
-   for search_word in All_words:
-      count = 0
-      for document in Corpus:
-         for word in document:
-            if search_word == word:
-               count += 1
-      #return search_word, count
-
-   return search_word, count
+    return [search_word, count]
 
 input = "happy"
 output = search(input) # invoke the method using a test input

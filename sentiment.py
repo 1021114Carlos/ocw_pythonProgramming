36b69aba4578efcacfeadb546978c440b04b2691
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-04-07 18:21:43 -0600

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,36 +1,35 @@
-def search(keyword):
-    #implement the function here
-    class Entry:
-        def __init__(self, word, synonyms):
-            self.word = word
-            self.synonyms = synonyms
-            
-    e1 = Entry("dog", ["cheerful", "mad", "delighted"])
-    e2 = Entry("nature", ["tiger", "waterfall", "joyful"])
+def search(keyword) :
+   # implement the function here
+   class Entry:
+      def __init__(self, word, synonyms):
+         self.word = word
+         self.synonyms = synonyms
 
-    Thesaurus = [e1, e2]
-    
-    doc1 = ["this", "is", "a", "single", "document"]
-    doc2 = ["here", "is", "another", "document"]
-    
-    Corpus = [doc1, doc2]
-    
-    word_list = []
-    All_words = [keyword]
-    for Entry in Thesaurus:
-        if keyword == Entry.word:
-            if word in Entry.synonyms:
-                All_words.append(word)
-            break
-    for search_word in All_words:
-        count = 0
-        for document in Corpus:
-            for word in document:
-                if search_word == word:
-                    count += 1
-                    word_list.append(search_word)
+   #e1 = Entry("dog", ["doggie", "puppy"])
+   #e2 = Entry("cat", ["kitty"])
 
-    return [search_word, count]
+   #Thesaurus = [e1, e2]
+
+   #doc1 = ["this", "is", "a", "single", "document"]
+   #doc2 = ["here", "is", "another", "document"]
+
+   #Corpus = [doc1, doc2]
+
+   final = []
+   All_words = [keyword]
+   for Entry in Thesaurus:
+      if keyword == Entry.word:
+         for word in Entry.synonyms:
+            All_words.append(word)
+   for keyword in All_words:
+      count = 0
+      for document in Corpus:
+         for word in document:
+               if keyword == word:
+                  count += 1
+                  result = (keyword, count)
+      final.append(result)
+   return final
 
 input = "happy"
 output = search(input) # invoke the method using a test input
